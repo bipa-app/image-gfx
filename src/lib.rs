6eadc2aad5a29f5050df95a5a9b9f02882c011b4
Felipe Rosa <felipe.sgrosa@gmail.com>
2021-11-25 20:53:59 -0300

    Antialiasing kinda working

--- src/lib.rs
+++ src/lib.rs
@@ -116,7 +116,7 @@ impl<I: GenericImage> Renderer for BasicRenderer<I> {
         let (mut x, mut y) = (x0 as i64, y0 as i64);
 
         loop {
-            img.put_pixel(x as u32, y as u32, color);
+            img.blend_pixel(x as u32, y as u32, color);
 
             if x as u32 == x1 && y as u32 == y1 {
                 break;
@@ -138,20 +138,20 @@ impl<I: GenericImage> Renderer for BasicRenderer<I> {
 
     fn draw_rect(&self, img: &mut Self::Image, rect: Rect, color: Self::Pixel) {
         for x in rect.left()..=rect.right() {
-            img.put_pixel(x, rect.top(), color);
-            img.put_pixel(x, rect.bottom(), color);
+            img.blend_pixel(x, rect.top(), color);
+            img.blend_pixel(x, rect.bottom(), color);
         }
 
         for y in rect.top()..=rect.bottom() {
-            img.put_pixel(rect.left(), y, color);
-            img.put_pixel(rect.right(), y, color);
+            img.blend_pixel(rect.left(), y, color);
+            img.blend_pixel(rect.right(), y, color);
         }
     }
 
     fn draw_filled_rect(&self, img: &mut Self::Image, rect: Rect, color: Self::Pixel) {
         for x in rect.left()..=rect.right() {
             for y in rect.top()..=rect.bottom() {
-                img.put_pixel(x, y, color);
+                img.blend_pixel(x, y, color);
             }
         }
     }
@@ -170,14 +170,14 @@ impl<I: GenericImage> Renderer for BasicRenderer<I> {
         let mut p = 1 - radius as i64;
 
         while x <= y {
-            img.put_pixel(x0 + x, y0 + y, color);
-            img.put_pixel(x0 + y, y0 + x, color);
-            img.put_pixel(x0 - y, y0 + x, color);
-            img.put_pixel(x0 - x, y0 + y, color);
-            img.put_pixel(x0 - x, y0 - y, color);
-            img.put_pixel(x0 - y, y0 - x, color);
-            img.put_pixel(x0 + y, y0 - x, color);
-            img.put_pixel(x0 + x, y0 - y, color);
+            img.blend_pixel(x0 + x, y0 + y, color);
+            img.blend_pixel(x0 + y, y0 + x, color);
+            img.blend_pixel(x0 - y, y0 + x, color);
+            img.blend_pixel(x0 - x, y0 + y, color);
+            img.blend_pixel(x0 - x, y0 - y, color);
+            img.blend_pixel(x0 - y, y0 - x, color);
+            img.blend_pixel(x0 + y, y0 - x, color);
+            img.blend_pixel(x0 + x, y0 - y, color);
 
             x += 1;
             if p < 0 {
@@ -245,9 +245,9 @@ impl<I> Default for AntiAliasingRender<I> {
     }
 }
 
-impl<I: GenericImage> Renderer for AntiAliasingRender<I> {
-    type Image = I;
-    type Pixel = <I as GenericImageView>::Pixel;
+impl Renderer for AntiAliasingRender<RgbaImage> {
+    type Image = RgbaImage;
+    type Pixel = <RgbaImage as GenericImageView>::Pixel;
 
     fn draw_line(
         &self,
@@ -256,15 +256,228 @@ impl<I: GenericImage> Renderer for AntiAliasingRender<I> {
         to: (u32, u32),
         color: Self::Pixel,
     ) {
+        let (steep, x0, x1, y0, y1) = {
+            let (mut x0, mut y0) = (from.0 as i64, from.1 as i64);
+            let (mut x1, mut y1) = (to.0 as i64, to.1 as i64);
+
+            let steep = (y1 - y0).abs() > (x1 - x0).abs();
+
+            if steep {
+                std::mem::swap(&mut x0, &mut y0);
+                std::mem::swap(&mut x1, &mut y1);
+            }
+
+            if x0 > x1 {
+                std::mem::swap(&mut x0, &mut x1);
+                std::mem::swap(&mut y0, &mut y1);
+            }
+
+            (steep, x0, x1, y0, y1)
+        };
+
+        let dx = x1 - x0;
+        let gradient = if dx == 0 {
+            1f64
+        } else {
+            (y1 - y0) as f64 / dx as f64
+        };
+
+        let mut intersect_y = y0 as f64;
+
+        if steep {
+            for x in x0..=x1 {
+                img.blend_pixel(
+                    intersect_y as u32,
+                    x as u32,
+                    rgba_u8_pixel_with_brightness(color, 1f64 - intersect_y.fract()),
+                );
+                img.blend_pixel(
+                    intersect_y as u32 + 1,
+                    x as u32,
+                    rgba_u8_pixel_with_brightness(color, intersect_y.fract()),
+                );
+                intersect_y += gradient;
+            }
+        } else {
+            for x in x0..=x1 {
+                img.blend_pixel(
+                    x as u32,
+                    intersect_y as u32,
+                    rgba_u8_pixel_with_brightness(color, 1f64 - intersect_y.fract()),
+                );
+                img.blend_pixel(
+                    x as u32,
+                    intersect_y as u32 + 1,
+                    rgba_u8_pixel_with_brightness(color, intersect_y.fract()),
+                );
+                intersect_y += gradient;
+            }
+        }
     }
 
-    fn draw_rect(&self, img: &mut Self::Image, rect: Rect, color: Self::Pixel) {}
+    fn draw_rect(&self, img: &mut Self::Image, rect: Rect, color: Self::Pixel) {
+        BasicRenderer::default().draw_rect(img, rect, color);
+    }
 
-    fn draw_filled_rect(&self, img: &mut Self::Image, rect: Rect, color: Self::Pixel) {}
+    fn draw_filled_rect(&self, img: &mut Self::Image, rect: Rect, color: Self::Pixel) {
+        BasicRenderer::default().draw_filled_rect(img, rect, color);
+    }
 
-    fn draw_circle(&self, img: &mut Self::Image, circle: Circle, color: Self::Pixel) {}
+    fn draw_circle(&self, img: &mut Self::Image, circle: Circle, color: Self::Pixel) {
+        let radius = circle.radius();
+        if radius == 0 {
+            return;
+        }
 
-    fn draw_filled_circle(&self, img: &mut Self::Image, circle: Circle, color: Self::Pixel) {}
+        let center = circle.center();
+        let (x0, y0) = center;
+
+        img.blend_pixel(x0 + radius, y0, color);
+        img.blend_pixel(x0 - radius, y0, color);
+        img.blend_pixel(x0, y0 + radius, color);
+        img.blend_pixel(x0, y0 - radius, color);
+
+        let mut x = radius;
+        let mut y = 0;
+
+        while x > y {
+            y += 1;
+
+            let real_x = ((radius * radius - y * y) as f64).sqrt();
+            let real_x_ceil = real_x.ceil();
+            x = real_x_ceil as u32;
+            let k = real_x_ceil - real_x;
+
+            let c1 = rgba_u8_pixel_with_brightness(color, 1f64 - k);
+            let c2 = rgba_u8_pixel_with_brightness(color, k);
+
+            img.blend_pixel(x0 + x, y0 + y, c1);
+            img.blend_pixel(x0 + x - 1, y0 + y, c2);
+
+            img.blend_pixel(x0 + y, y0 + x, c1);
+            img.blend_pixel(x0 + y, y0 + x - 1, c2);
+
+            img.blend_pixel(x0 - y, y0 + x, c1);
+            img.blend_pixel(x0 - y, y0 + x - 1, c2);
+
+            img.blend_pixel(x0 - x, y0 + y, c1);
+            img.blend_pixel(x0 - x + 1, y0 + y, c2);
+
+            img.blend_pixel(x0 - x, y0 - y, c1);
+            img.blend_pixel(x0 - x + 1, y0 - y, c2);
+
+            img.blend_pixel(x0 - y, y0 - x, c1);
+            img.blend_pixel(x0 - y, y0 - x + 1, c2);
+
+            img.blend_pixel(x0 + y, y0 - x, c1);
+            img.blend_pixel(x0 + y, y0 - x + 1, c2);
+
+            img.blend_pixel(x0 + x, y0 - y, c1);
+            img.blend_pixel(x0 + x - 1, y0 - y, c2);
+        }
+    }
+
+    fn draw_filled_circle(&self, img: &mut Self::Image, circle: Circle, color: Self::Pixel) {
+        let radius = circle.radius();
+        if radius == 0 {
+            return;
+        }
+
+        let center = circle.center();
+        let (x0, y0) = center;
+
+        img.blend_pixel(x0 + radius, y0, color);
+        img.blend_pixel(x0 - radius, y0, color);
+        img.blend_pixel(x0, y0 + radius, color);
+        img.blend_pixel(x0, y0 - radius, color);
+
+        self.draw_line(img, (x0 - radius, y0), (x0 + radius, y0), color);
+        self.draw_line(img, (x0, y0 - radius), (x0, y0 + radius), color);
+
+        let mut x = radius;
+        let mut y = 0;
+
+        while x > y {
+            y += 1;
+
+            let real_x = ((radius * radius - y * y) as f64).sqrt();
+            let real_x_ceil = real_x.ceil();
+            x = real_x_ceil as u32;
+            let k = real_x_ceil - real_x;
+
+            let c1 = rgba_u8_pixel_with_brightness(color, 1f64 - k);
+            let c2 = rgba_u8_pixel_with_brightness(color, k);
+
+            img.blend_pixel(x0 + x, y0 + y, c1);
+            img.blend_pixel(x0 + x - 1, y0 + y, c2);
+
+            img.blend_pixel(x0 + y, y0 + x, c1);
+            img.blend_pixel(x0 + y, y0 + x - 1, c2);
+
+            img.blend_pixel(x0 - y, y0 + x, c1);
+            img.blend_pixel(x0 - y, y0 + x - 1, c2);
+
+            img.blend_pixel(x0 - x, y0 + y, c1);
+            img.blend_pixel(x0 - x + 1, y0 + y, c2);
+
+            img.blend_pixel(x0 - x, y0 - y, c1);
+            img.blend_pixel(x0 - x + 1, y0 - y, c2);
+
+            img.blend_pixel(x0 - y, y0 - x, c1);
+            img.blend_pixel(x0 - y, y0 - x + 1, c2);
+
+            img.blend_pixel(x0 + y, y0 - x, c1);
+            img.blend_pixel(x0 + y, y0 - x + 1, c2);
+
+            img.blend_pixel(x0 + x, y0 - y, c1);
+            img.blend_pixel(x0 + x - 1, y0 - y, c2);
+
+            if x0 >= x {
+                self.draw_line(
+                    img,
+                    ((x0 - x + 1), (y0 + y)),
+                    ((x0 + x - 1), (y0 + y)),
+                    color,
+                );
+            }
+
+            if x0 >= y {
+                self.draw_line(
+                    img,
+                    ((x0 - y), (y0 + x - 1)),
+                    ((x0 + y), (y0 + x - 1)),
+                    color,
+                );
+            }
+
+            if x0 >= x && y0 >= y {
+                self.draw_line(
+                    img,
+                    ((x0 - x + 1), (y0 - y)),
+                    ((x0 + x - 1), (y0 - y)),
+                    color,
+                );
+            }
+
+            if x0 >= y && y0 >= x {
+                self.draw_line(
+                    img,
+                    ((x0 - y), (y0 - x + 1)),
+                    ((x0 + y), (y0 - x + 1)),
+                    color,
+                );
+            }
+        }
+    }
+}
+
+fn rgba_u8_pixel_with_brightness(pixel: Rgba<u8>, brightness: f64) -> Rgba<u8> {
+    Rgba([
+        pixel.0[0],
+        pixel.0[1],
+        pixel.0[2],
+        (255.0 * brightness) as u8,
+    ])
 }
 
 #[cfg(test)]
@@ -276,9 +489,12 @@ mod tests {
         let mut img = image::RgbaImage::from_pixel(100, 100, image::Rgba([255, 255, 255, 255]));
 
         let r = BasicRenderer::default();
+        let aar = AntiAliasingRender::default();
 
         r.draw_line(&mut img, (90, 70), (90, 80), image::Rgba([0, 0, 255, 255]));
         r.draw_line(&mut img, (90, 70), (80, 60), image::Rgba([0, 0, 255, 255]));
+
+        aar.draw_line(&mut img, (97, 70), (80, 60), image::Rgba([255, 0, 0, 255]));
 
         r.draw_rect(
             &mut img,
@@ -292,10 +508,27 @@ mod tests {
             image::Rgba([255, 0, 0, 255]),
         );
 
+        aar.draw_filled_circle(
+            &mut img,
+            Circle::new((50, 50), 30),
+            image::Rgba([0, 0, 255, 255]),
+        );
+        r.draw_circle(
+            &mut img,
+            Circle::new((50, 50), 30),
+            image::Rgba([255, 0, 0, 255]),
+        );
+
         r.draw_circle(
             &mut img,
             Circle::new((50, 50), 20),
             image::Rgba([0, 255, 0, 255]),
+        );
+
+        aar.draw_circle(
+            &mut img,
+            Circle::new((50, 50), 15),
+            image::Rgba([255, 0, 255, 255]),
         );
 
         r.draw_filled_circle(
